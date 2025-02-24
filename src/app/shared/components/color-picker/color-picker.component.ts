ec8090c9551b547a65f0c7166e5f66557b9c8ca6
Paweł Kubiak <pawel.kubiak.dev@gmail.com>
2025-02-24 13:53:03 +0100

    feat: default colors

--- src/app/shared/components/color-picker/color-picker.component.ts
+++ src/app/shared/components/color-picker/color-picker.component.ts
@@ -24,6 +24,16 @@ export class ColorPickerComponent implements ControlValueAccessor {
   isDialogVisible = false;
   tempColor: string | null = null;
 
+  defaultColors: string[] = [
+    '#FF0000', '#FF4500', '#FFA500', '#FFD700', '#FFFF00', '#9ACD32', '#008000', '#00FF00',
+    '#00FA9A', '#00FFFF', '#00BFFF', '#0000FF', '#8A2BE2', '#4B0082', '#800080', '#FF00FF',
+    '#FF69B4', '#FFB6C1', '#FFF0F5', '#F0FFF0', '#F0FFFF', '#F5F5F5', '#DCDCDC', '#D3D3D3',
+    '#C0C0C0', '#A9A9A9', '#808080', '#696969', '#000000', '#800000', '#8B4513', '#A0522D',
+    '#BC8F8F', '#DEB887', '#F4A460', '#DAA520', '#EEE8AA', '#BDB76B', '#F0E68C', '#808000',
+    '#556B2F', '#006400', '#008B8B', '#2F4F4F', '#483D8B', '#191970', '#4B0082', '#8B008B',
+    '#8B0000', '#CD853F', '#D2691E', '#8B4513', '#A52A2A', '#B8860B', '#006400', '#483D8B'
+  ];
+
   private onChange: (value: string | null) => void = () => {};
   private onTouched: () => void = () => {};
 
@@ -53,5 +63,9 @@ export class ColorPickerComponent implements ControlValueAccessor {
     this.onChange(this.selectedColor);
     this.onTouched();
     this.isDialogVisible = false;
+  }
+
+  selectDefaultColor(color: string): void {
+    this.tempColor = color;
   }
 }
